0ff61b570b18c629ccb0a008b2b0440fed802ff5
puppycat710 <puppycat710@proton.me>
2025-05-21 14:49:12 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const { src, dest, series } = require('gulp');
+const uglify = require('gulp-uglify');
+const minifyHtml = require('gulp-minify-html');
+const cssnano = require('gulp-cssnano');
+
+// Minify HTML
+const buildHTML = () => {
+  return src("./*.html")  // Caminho ajustado
+    .pipe(minifyHtml())
+    .pipe(dest("./")); // Sobrescreve os arquivos originais
+}
+// CSSNano
+const buildCSS = () => {
+  return src("./css/*.css")
+    .pipe(cssnano())
+    .pipe(dest("./css/"));
+}
+/** 
+// Babel
+const buildBabel = () => {
+  return src('./js/*.js')
+    .pipe(babel())
+    .pipe(dest('./js/'));
+};
+**/
+// Uglify
+const buildJS = () => {
+  return src('./js/*.js')
+    .pipe(uglify())
+    .pipe(dest('./js/'));
+};
+
+exports.buildDev = series(buildHTML, buildJS, buildCSS);
---
+++ src/index.md
@@ -0,0 +1,6 @@
+---
+title: 'Soluções Inteligentes em Benefícios Corporativos e Planos de Saúde | Tuali'
+layout: layout.njk
+---
+
+{%- include "home.njk" -%}
