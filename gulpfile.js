0f8403056f0ab3094555ad1f65bd2a794b1cd508
puppycat710 <puppycat710@proton.me>
2025-05-22 12:00:34 -0300

    Finish code

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,17 @@
-const { src, dest, series } = require('gulp');
-const uglify = require('gulp-uglify');
-const minifyHtml = require('gulp-minify-html');
-const cssnano = require('gulp-cssnano');
+const { src, dest, series } = require('gulp')
+// const uglify = require('gulp-uglify')
+const minifyHtml = require('gulp-minify-html')
+const cssnano = require('gulp-cssnano')
 
 // Minify HTML
 const buildHTML = () => {
-  return src("./*.html")  // Caminho ajustado
-    .pipe(minifyHtml())
-    .pipe(dest("./")); // Sobrescreve os arquivos originais
+	return src('./*.html') // Caminho ajustado
+		.pipe(minifyHtml())
+		.pipe(dest('./')) // Sobrescreve os arquivos originais
 }
 // CSSNano
 const buildCSS = () => {
-  return src("./css/*.css")
-    .pipe(cssnano())
-    .pipe(dest("./css/"));
+	return src('./css/*.css').pipe(cssnano()).pipe(dest('./css/'))
 }
 /** 
 // Babel
@@ -22,12 +20,13 @@ const buildBabel = () => {
     .pipe(babel())
     .pipe(dest('./js/'));
 };
-**/
+
 // Uglify
 const buildJS = () => {
   return src('./js/*.js')
     .pipe(uglify())
     .pipe(dest('./js/'));
 };
+**/
 
-exports.buildDev = series(buildHTML, buildJS, buildCSS);
+exports.buildDev = series(buildHTML, buildCSS)
